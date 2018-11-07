837bb70cd6ea006a51bac4e82685dff0fc32dfda
MarcosGaming <40284094@live.napier.ac.uk>
2018-11-07 19:07:20 +0000

    Practical 7 needs revision

--- Practical 7/main.cpp
+++ Practical 7/main.cpp
@@ -56,7 +56,7 @@ int main()
 	#pragma region Task_2_variables
 
 	// Set up a cubic rigid body
-	RigidBody rb = RigidBody();
+	/*RigidBody rb = RigidBody();
 	Mesh m = Mesh::Mesh(Mesh::CUBE);
 	rb.setMesh(m);
 	Shader rbShader = Shader("resources/shaders/physics.vert ", "resources/shaders/physics.frag ");
@@ -67,22 +67,40 @@ int main()
 	// rigid body motion values
 	rb.translate(glm::vec3(0.0f, 5.0f, 0.0f));
 	rb.setVel(glm::vec3(3.0f, 0.0f, 0.0f));
-	rb.setAngVel(glm::vec3(0.0f, 0.0f, 0.0f));
+	rb.setAngVel(glm::vec3(0.0f, 0.0f, 1.0f));
 
 	// impulses
 	bool impulsed = false;
-	glm::vec3 j = glm::vec3(-3.0f, 0.0f, 0.0f);
-
-	// add forces to Rigid body
-	//rb.addForce(fgravity);
+	glm::vec3 j = glm::vec3(-3.0f, 0.0f, 0.0f);*/
 
 	#pragma endregion Task_2_variables
 
-	#pragma region Task_3_And_4_variables
+	#pragma region Task_3_variables
 
+	// Set up a cubic rigid body
+	RigidBody rb = RigidBody();
+	Mesh m = Mesh::Mesh(Mesh::CUBE);
+	rb.setMesh(m);
+	Shader rbShader = Shader("resources/shaders/physics.vert ", "resources/shaders/physics.frag ");
+	rb.getMesh().setShader(rbShader);
+	rb.setMass(1.0f);
+	rb.scale(glm::vec3(1.0f, 3.0f, 1.0f));
 
+	// rigid body motion values
+	rb.translate(glm::vec3(0.0f, 5.0f, 0.0f));
+	rb.setVel(glm::vec3(0.0f, 0.0f, 0.0f));
+	rb.setAngVel(glm::vec3(0.0f, 0.0f, 0.0f));
 
-	#pragma endregion Task_3_And_4_variables
+	// add forces
+	rb.addForce(fgravity);
+
+	// impulse elements
+	float e = 1.0f;
+	bool collision = false;
+	int vertexCount = 0;
+	glm::vec3 *verticesInCollision[4];
+
+	#pragma endregion Task_3_variables
 	
 	// Game loop
 	while (!glfwWindowShouldClose(app.getWindow()))
@@ -107,7 +125,7 @@ int main()
 		{
 			#pragma region Task_2_simulation
 			// integration (translation)
-			rb.setAcc(rb.applyForces(rb.getPos(), rb.getVel(), t, deltaTime));
+			/*rb.setAcc(rb.applyForces(rb.getPos(), rb.getVel(), t, deltaTime));
 			rb.setVel(rb.getVel() + deltaTime * rb.getAcc());
 			rb.translate(rb.getVel() * deltaTime);
 
@@ -136,18 +154,78 @@ int main()
 			// impulses
 			if (currentTime > 2.0f && !impulsed)
 			{
-				rb.setVel(j / rb.getMass());
+				rb.setVel(rb.getVel() + j / rb.getMass());
+				rb.setAngVel(rb.getInvInertia() * glm::cross(rb.getPos() + glm::vec3(1.0f, 0.0f, 0.0f) - rb.getPos(), j));
 				impulsed = true;
-			}
-			//rb.setAngVel(rb.getInvInertia() * (glm::cross(rb.getPos() + glm::vec3(1.0, 1.0f,0) - rb.getPos(), j)));
+			}*/
 
 			#pragma endregion Task_2_simulation
 
-			#pragma region Task_3_And_4_simulation
+			#pragma region Task_3_simulation
 
+			// integration (translation)
+			rb.setAcc(rb.applyForces(rb.getPos(), rb.getVel(), t, deltaTime));
+			rb.setVel(rb.getVel() + deltaTime * rb.getAcc());
+			rb.translate(rb.getVel() * deltaTime);
 
+			// integration ( rotation )
+			rb.setAngVel(rb.getAngVel() + deltaTime * rb.getAngAcc());
+			// create skew symmetric matrix for w
+			glm::mat3 angVelSkew = glm::matrixCross3(rb.getAngVel());
+			// create 3x3 rotation matrix from rb rotation matrix
+			glm::mat3 R = glm::mat3(rb.getRotate());
+			// update rotation matrix
+			R += deltaTime * angVelSkew *R;
+			R = glm::orthonormalize(R);
+			rb.setRotate(glm::mat4(R));
 
-			#pragma endregion Task_3_And_4_simulation
+			// Check how many vertex are in collision with the plane 
+			for (auto vertex : rb.getMesh().getVertices())
+			{
+				glm::vec3 coordinates = rb.getMesh().getModel() * glm::vec4(vertex.getCoord(), 1.0f);
+				if (coordinates.y <= plane.getPos().y)
+				{
+					verticesInCollision[vertexCount] = &coordinates;
+					vertexCount++;
+					collision = true;
+				}
+			}
+			if(collision)
+			{
+				glm::vec3 collisionPoint;
+				// Vertex
+				if (vertexCount == 1)
+				{
+					collisionPoint = *verticesInCollision[0];
+				}
+				// Edge
+				else if (vertexCount == 2)
+				{
+					collisionPoint = (*verticesInCollision[0] + *verticesInCollision[1]) / 2;
+				}
+				// Face
+				else if (vertexCount == 4)
+				{
+					collisionPoint = glm::vec3(rb.getPos().x, rb.getPos().y - 3.0f, rb.getPos().z);
+				}
+				vertexCount = 0;
+				collision = false;
+				// Translate the rigid body up a bit so it does not overlap the plane
+				rb.translate(glm::vec3(0.0f, 0.5f, 0.0f));
+				// r is the vector of the centre of mass and the point of collision
+				glm::vec3 r = collisionPoint - rb.getPos();
+				// n is the normal of the plane of collision, in this case the normal of the plane
+				glm::vec3 n = glm::vec3(0.0f, 1.0f, 0.0f);
+				// vr is the relative velocity
+				glm::vec3 vr = rb.getVel() + glm::cross(rb.getAngVel(), r);
+				// calculate the impulse
+				glm::vec3 jr = (-(1.0f + e) * vr * n) / ((1 / rb.getMass()) + n * (rb.getInvInertia()*glm::cross(glm::cross(r, n), r)));
+				// set the new velocities
+				rb.setVel(rb.getVel() + (jr / rb.getMass())*n);
+				rb.setAngVel(rb.getAngVel() + jr * rb.getInvInertia() * glm::cross(r, n));
+			}
+
+			#pragma endregion Task_3_simulation
 
 			accumulator -= deltaTime;
 			t += deltaTime;
@@ -163,15 +241,15 @@ int main()
 
 		#pragma region Task_2_draw
 
-		app.draw(rb.getMesh());
+		//app.draw(rb.getMesh());
 
 		#pragma endregion Task_2_draw
 
-		#pragma region Task_3_And_4_draw
+		#pragma region Task_3_draw
 
-		
+		app.draw(rb.getMesh());
 
-		#pragma endregion Task_3_And_4_draw
+		#pragma endregion Task_3_draw
 
 
 		app.display();
